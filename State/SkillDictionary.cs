b7e042cc8bd8da99b07abf2dee2d44d0ddfe8cf8
Brad Green <greenbrad@users.noreply.github.com>
2023-12-10 12:58:46 -0500

    Update SkillDictionary.cs
    Add Eldritch Battery functionality to CanBeUsed

--- State/SkillDictionary.cs
+++ State/SkillDictionary.cs
@@ -13,26 +13,31 @@ public class SkillDictionary
 {
     private readonly Lazy<Dictionary<string, SkillInfo>> _source;
 
-    public SkillDictionary(GameController controller, Actor actor, Life lifeComponent)
+public SkillDictionary(GameController controller, Actor actor, Life lifeComponent)
+{
+    if (actor == null)
     {
-        if (actor == null)
-        {
-            _source = new Lazy<Dictionary<string, SkillInfo>>(new Dictionary<string, SkillInfo>());
-        }
-        else
-        {
-            _source = new Lazy<Dictionary<string, SkillInfo>>(() => actor.ActorSkills
-                .Where(x => !string.IsNullOrWhiteSpace(x.Name))
-                .DistinctBy(x => x.Name, StringComparer.OrdinalIgnoreCase)
-                .Select(x => new SkillInfo(true, x.Name, x.CanBeUsed && x.Cost <= (lifeComponent?.CurMana ?? 10000), x.GetStat(ExileCore.Shared.Enums.GameStat.LifeCost),
-                    new Lazy<List<MonsterInfo>>(() => x.DeployedObjects.Select(d => d?.Entity)
-                            .Where(e => e != null)
-                            .Select(e => new MonsterInfo(controller, e))
-                            .ToList(),
-                        LazyThreadSafetyMode.None)))
-                .ToDictionary(x => x.Name, StringComparer.OrdinalIgnoreCase), LazyThreadSafetyMode.None);
-        }
+        _source = new Lazy<Dictionary<string, SkillInfo>>(new Dictionary<string, SkillInfo>());
     }
+    else
+    {
+        var localPlayer = controller.Game.IngameState.Data.LocalPlayer;
+        var eldritchBatteryTaken = localPlayer.Stats.TryGetValue(GameStat.VirtualEnergyShieldProtectsMana, out var value) && value > 0;
+
+        _source = new Lazy<Dictionary<string, SkillInfo>>(() => actor.ActorSkills
+            .Where(x => !string.IsNullOrWhiteSpace(x.Name))
+            .DistinctBy(x => x.Name, StringComparer.OrdinalIgnoreCase)
+            .Select(x => new SkillInfo(true, x.Name, 
+                x.CanBeUsed && x.Cost <= (eldritchBatteryTaken && lifeComponent != null ? lifeComponent?.CurES + lifeComponent.CurMana : lifeComponent?.CurMana ?? 10000), 
+                x.GetStat(ExileCore.Shared.Enums.GameStat.LifeCost),
+                new Lazy<List<MonsterInfo>>(() => x.DeployedObjects.Select(d => d?.Entity)
+                        .Where(e => e != null)
+                        .Select(e => new MonsterInfo(controller, e))
+                        .ToList(),
+                    LazyThreadSafetyMode.None)))
+            .ToDictionary(x => x.Name, StringComparer.OrdinalIgnoreCase), LazyThreadSafetyMode.None);
+    }
+}
 
     [Api]
     public SkillInfo this[string id]
