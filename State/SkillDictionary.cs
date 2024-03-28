0134608d3030582666ef590a37ac1fbd9d36cce9
Arecurius <36637378+Arecurius0@users.noreply.github.com>
2024-03-28 22:22:23 +0100

    Added Cooldown timer info to SkillInfo

--- State/SkillDictionary.cs
+++ State/SkillDictionary.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using ExileCore;
+﻿using ExileCore;
 using ExileCore.PoEMemory.Components;
 using ExileCore.PoEMemory.MemoryObjects;
 using ExileCore.Shared.Enums;
 using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 
 namespace ReAgent.State;
 
@@ -36,6 +36,14 @@ public class SkillDictionary
                 : eldritchBatteryTaken
                     ? lifeComponent.CurHP
                     : lifeComponent.CurES + lifeComponent.CurHP;
+
+            var actorSkillsCooldownDict = actor.ActorSkills.Join(
+                actor.ActorSkillsCooldowns,
+                skill => skill.Id,
+                cooldown => cooldown.Id,
+                (skill, cooldown) => new { skill.InternalName, cooldown })
+                .ToDictionary(item => item.InternalName, item => item.cooldown);
+
             _source = new Lazy<Dictionary<string, SkillInfo>>(() => actor.ActorSkills
                 .Where(x => !string.IsNullOrWhiteSpace(x.Name))
                 .DistinctBy(x => x.Name, StringComparer.OrdinalIgnoreCase)
@@ -45,6 +53,7 @@ public class SkillDictionary
                     x.Cost <= currentManaPool &&
                     x.GetStat(GameStat.LifeCost) < currentHpPool,
                     x.GetStat(GameStat.LifeCost),
+                    actorSkillsCooldownDict.ContainsKey(x.InternalName) ? actorSkillsCooldownDict[x.InternalName] : null,
                     new Lazy<List<MonsterInfo>>(() => x.DeployedObjects.Select(d => d?.Entity)
                             .Where(e => e != null)
                             .Select(e => new MonsterInfo(controller, e))
@@ -64,7 +73,7 @@ public class SkillDictionary
                 return value;
             }
 
-            return new SkillInfo(false, id, false, 0, new Lazy<List<MonsterInfo>>([]));
+            return new SkillInfo(false, id, false, 0, null, new Lazy<List<MonsterInfo>>([]));
         }
     }
 
--- State/SkillInfo.cs
+++ State/SkillInfo.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using ExileCore.PoEMemory.MemoryObjects;
+using System;
 using System.Collections.Generic;
 
 namespace ReAgent.State;
 
 [Api]
-public record SkillInfo([property: Api] bool Exists, [property: Api] string Name, [property: Api] bool CanBeUsed, [property: Api] int LifeCost, Lazy<List<MonsterInfo>> DeployedEntitiesFunc)
+public record SkillInfo([property: Api] bool Exists, [property: Api] string Name, [property: Api] bool CanBeUsed, [property: Api] int LifeCost, ActorSkillCooldown Cooldown, Lazy<List<MonsterInfo>> DeployedEntitiesFunc)
 {
     [Api]
     public List<MonsterInfo> DeployedEntities => DeployedEntitiesFunc.Value;
